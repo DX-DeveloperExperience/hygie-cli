3a0e63d0617278e5f662fe499b2dd1996668d43b
TERRIER Bastien <bastien.terrier@gmail.com>
2019-04-02 10:41:19 +0200

    feat: add spec file when creating new rule (#1)

--- src/rule/rule.factory.ts
+++ src/rule/rule.factory.ts
@@ -10,9 +10,15 @@ export function main(options: RuleOptions): Rule {
     tree.overwrite(`src/rules/index.ts`, indexFileContent(ruleName, fileName));
 
     // Create the Rule file
-    return tree.create(
+    tree.create(
       `src/rules/${fileName}.rule.ts`,
       ruleFileContent(ruleName, fileName),
+    );
+
+    // Create the Rule Test file
+    return tree.create(
+      `src/rules/${fileName}.rule.spec.ts`,
+      ruleTestFileContent(ruleName, fileName),
     );
   };
 }
@@ -67,4 +73,50 @@ function indexFileContent(ruleName: string, fileName: string): string {
   const newExport: string = `export { ${ruleName}Rule } from './${fileName}.rule';\n`;
 
   return fileContent + newExport;
+}
+
+function ruleTestFileContent(ruleName: string, fileName: string): string {
+  return `import { Test, TestingModule } from '@nestjs/testing';
+import { GithubService } from '../github/github.service';
+import { GitlabService } from '../gitlab/gitlab.service';
+import { Webhook } from '../webhook/webhook';
+import { RuleResult } from '../rules/ruleResult';
+import { HttpService } from '@nestjs/common';
+import {
+  MockHttpService,
+  MockGitlabService,
+  MockGithubService,
+} from '../__mocks__/mocks';
+import { ${ruleName}Rule } from './${fileName}.rule';
+
+describe('RulesService', () => {
+  let app: TestingModule;
+  let githubService: GithubService;
+  let gitlabService: GitlabService;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      providers: [
+        { provide: HttpService, useClass: MockHttpService },
+        { provide: GitlabService, useClass: MockGitlabService },
+        { provide: GithubService, useClass: MockGithubService },
+      ],
+    }).compile();
+
+    githubService = app.get(GithubService);
+    gitlabService = app.get(GitlabService);
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // ${ruleName} Rule
+  describe('${fileName} Rule', () => {
+    it('should do something', () => {
+      // Implements your tests here
+    });
+  });
+});
+`;
 }
